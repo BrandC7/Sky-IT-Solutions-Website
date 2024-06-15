f7fc71e4fd124399f792cfddf0885f6db0a1cf0c
Brandon Cavazos <bcav47@gmail.com>
2024-06-15 16:28:29 -0500

    remove: comments

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //Next lines can be adapt for the deployment between Docker and Github.
-  // base: "/Sky-IT-Solutions-Website/",
-  //  preview: {
-  //    port: 3000,
-  //    strictPort: true,
-  //  },
-  //  server: {
-  //    port: 3000,
-  //    strictPort: true,
-  //    host: true,
-  //    origin: "http://localhost:3000",  
-  //  },
 });
